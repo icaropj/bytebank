8de37b6c1a5c5b3d05e5b696768c7bd436920941
Icaro Pinho Esmeraldo <iesmeraldo@e-deploy.local>
2020-05-20 11:05:42 -0300

    Contact list layout and navigation to it

---
+++ lib/screens/contact_list.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class ContactList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text(
+                'Icaro',
+                style: TextStyle(fontSize: 24),
+              ),
+              subtitle: Text(
+                '4455',
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -1,4 +1,4 @@
-
+import 'package:bytebank/screens/contact_list.dart';
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -18,30 +18,36 @@ class Dashboard extends StatelessWidget {
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Container(
-              padding: EdgeInsets.all(8),
-              height: 100,
-              width: 150,
-              color: Theme
-                  .of(context)
-                  .primaryColor,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Icon(
-                    Icons.people,
-                    color: Colors.white,
-                    size: 24,
+            child: Material(
+              color: Theme.of(context).primaryColor,
+              child: InkWell(
+                child: Container(
+                  padding: EdgeInsets.all(8),
+                  height: 100,
+                  width: 150,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Icon(
+                        Icons.people,
+                        color: Colors.white,
+                        size: 24,
+                      ),
+                      Text(
+                        'Contacts',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                        ),
+                      )
+                    ],
                   ),
-                  Text(
-                    'Contacts',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                  )
-                ],
+                ),
+                onTap: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => ContactList()));
+                },
               ),
             ),
           )
