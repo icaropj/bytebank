30c2b0a217559889c18e97c98f89d57d698ad763
Icaro Pinho Esmeraldo <iesmeraldo@e-deploy.local>
2020-05-20 11:14:18 -0300

    Create contact form layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank/screens/contact_form.dart';
 import 'package:bytebank/screens/dashboard.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
---
+++ lib/screens/contact_form.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class ContactForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Contact'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'Full name'
+                ),
+                style: TextStyle(
+                  fontSize: 24
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: 'Account number'
+                ),
+                style: TextStyle(
+                    fontSize: 24
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: RaisedButton(
+                  child: const Text('Create'),
+                  onPressed: () {},
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
