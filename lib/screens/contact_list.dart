37f1fe95a4d24981f486daac6aa56c3d2e48f7a0
Icaro Pinho Esmeraldo <iesmeraldo@e-deploy.local>
2020-05-27 10:58:14 -0300

    Save transfer on api from form

--- lib/screens/contact_list.dart
+++ lib/screens/contact_list.dart
@@ -2,6 +2,7 @@ import 'package:bytebank/components/progress.dart';
 import 'package:bytebank/database/dao/contact_dao.dart';
 import 'package:bytebank/models/contact.dart';
 import 'package:bytebank/screens/contact_form.dart';
+import 'package:bytebank/screens/transaction_form.dart';
 import 'package:flutter/material.dart';
 
 class ContactList extends StatefulWidget {
@@ -10,7 +11,6 @@ class ContactList extends StatefulWidget {
 }
 
 class _ContactListState extends State<ContactList> {
-
   final _contactDao = ContactDao();
 
   @override
@@ -54,7 +54,14 @@ class _ContactListState extends State<ContactList> {
     return ListView.builder(
       itemBuilder: (context, index) {
         final contact = contacts[index];
-        return _ContactItem(contact);
+        return _ContactItem(
+          contact,
+          onClick: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => TransactionForm(contact),
+            ));
+          },
+        );
       },
       itemCount: contacts.length,
     );
@@ -63,13 +70,15 @@ class _ContactListState extends State<ContactList> {
 
 class _ContactItem extends StatelessWidget {
   final Contact contact;
+  final Function onClick;
 
-  _ContactItem(this.contact);
+  _ContactItem(this.contact, {@required this.onClick});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       child: ListTile(
+        onTap: () => onClick(),
         title: Text(
           contact.name,
           style: TextStyle(fontSize: 24),
