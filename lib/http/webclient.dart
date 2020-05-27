72e3c757344da97fbc2d978558adbfe76e3156d1
Icaro Pinho Esmeraldo <iesmeraldo@e-deploy.local>
2020-05-27 10:40:10 -0300

    Handling api errors

--- lib/http/webclient.dart
+++ lib/http/webclient.dart
@@ -9,8 +9,9 @@ import 'package:http_interceptor/http_client_with_interceptor.dart';
 Future<List<Transaction>> findAll() async {
   final Client client =
       HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
-  final Response response =
-      await client.get('http://192.168.5.234:8080/transactions');
+  final Response response = await client
+      .get('http://192.168.5.234:8080/transactions')
+      .timeout(Duration(seconds: 10));
   final List<dynamic> decodedJson = jsonDecode(response.body);
   final List<Transaction> transactions = List();
   for (Map<String, dynamic> transactionJson in decodedJson) {
